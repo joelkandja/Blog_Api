08a0ccc89f4a50c0a4a11375e074dd24f26f9db1
joel kandja <j.kandja@evolve-rdc.com>
2024-05-04 11:56:45 +0100

    init project

---
+++ api/config/migrations.js
@@ -0,0 +1,11 @@
+// config/migrations.js
+
+
+
+
+
+
+module.exports.migrations = {
+    // connection name matches a field from config/connections.js
+    connection: 'default' // or MySQL
+  };
---
+++ api/tasks/register/dbMigrate.js
@@ -0,0 +1,1 @@
+module.exports = require( 'sails-grunt-task-db-migrate' ).sailsGruntTasks
